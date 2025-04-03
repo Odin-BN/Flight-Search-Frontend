1bec01bbb1e9afeee54ce50337287eb03db7e25f
Odin-BN <odin.berumen@encora.com>
2025-04-02 22:19:52 -0700

    Improving the sorting buttons, both categories sort together, still pending to be able to sort by one or another, adding context variables to fetch flights where is detected a change in the sorting buttons

--- src/components/ui/SortingButtons.tsx
+++ src/components/ui/SortingButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Button, Flex } from 'antd';
 import FlightsSort from '@/context/FlightsSort';
 import FlightList from '@/context/FlightList';
@@ -6,25 +6,31 @@ import FlightList from '@/context/FlightList';
 const SortingButtons: React.FC = () => {
   const {flightsSort} = useContext(FlightsSort);
   const {fetchFlights} = useContext(FlightList);
+  const {sortCalls} = useContext(FlightsSort);
 
   const sortPrice = () => {
     flightsSort("price");
 
-    setTimeout(() => {
+    /*setTimeout(() => {
       fetchFlights();
       console.log("Termino el tiempo antes de solicitar la lista sorteada por precios");
-    }, 5000);
+    }, 5000);*/
 
   };
 
   const sortDuration = () => {
     flightsSort("duration");
 
-    setTimeout(() => {
+    /*setTimeout(() => {
       fetchFlights();
       console.log("Termino el tiempo antes de solicitar la lista sorteada por duracion");
-    }, 5000);
+    }, 5000);*/
   }
+
+  useEffect(() => {
+    console.log("Se detecto cambio de orden por precio: " + sortCalls);
+    fetchFlights();
+  }, [sortCalls]);
 
   return (
   <Flex gap="small" wrap>
--- src/context/FlightsSort.tsx
+++ src/context/FlightsSort.tsx
@@ -5,16 +5,21 @@ type FlightsSortType = {
     priceOrder: string;
     setPriceOrder: (priceOrder: string) => void ;
     durationOrder: string;
+    sortCalls: number;
     setDurationOrder: (durationOrder: string) => void;
+    setSortCalls: (sortCalls: number) => void;
     flightsSort: (type: "price" | "duration") => void;
 }
 
 const FlightsSort = createContext<FlightsSortType>({
     priceOrder : "desc",
     durationOrder: "desc",
+    sortCalls: 0,
     setPriceOrder: () => {},
     setDurationOrder: () => {},
+    setSortCalls: () => {},
     flightsSort: () => {},
+
 });
 
 type FlightsSortProps = {
@@ -25,6 +30,7 @@ type FlightsSortProps = {
 export const FlightsSorting = ({children}: FlightsSortProps) => {
     const [priceOrder, setPriceOrder] = useState<string>("");
     const [durationOrder, setDurationOrder] = useState<string>("");
+    const [sortCalls, setSortCalls] = useState<number>(0);
 
     const flightsSort = async (type: "price" | "duration") => {
         let newOrder: "asc" | "desc";
@@ -57,14 +63,18 @@ export const FlightsSorting = ({children}: FlightsSortProps) => {
 
             if (!response.ok) {
                 throw new Error("Error ordenando la lista ordenada por parametros");
+            } else {
+                console.log("Se realizo con exito el orden por categoria")
+                setSortCalls(sortCalls + 1);
             }
+
         } catch (error) {
             console.error("Error enviando los datos de sorting al backend");
         }
     }
 
     return (
-        <FlightsSort.Provider value = {{priceOrder, setPriceOrder, durationOrder, setDurationOrder, flightsSort}}>
+        <FlightsSort.Provider value = {{priceOrder, setPriceOrder, durationOrder, setDurationOrder, sortCalls, setSortCalls, flightsSort}}>
 
                 {children}
 
